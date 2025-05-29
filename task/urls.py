a6bfd19d21ebc04bf932f1304340aee107f43c9a
Refat Jamil <s.m.refat.jamil@gmail.com>
2025-05-29 23:10:26 +0600

    Added task list view, created templates and URLs, view task list in template

--- task/urls.py
+++ task/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('my-task', views.task_list, name='my_task')
+
 ]
--- task/views.py
+++ task/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from .models import Task
+from django.contrib.auth.decorators import login_required
 
 def home(request):
     return render(request, 'home.html')
+
+
+@login_required
+def task_list(request):
+    tasks = Task.objects.filter(user=request.user)
+    context = {'task_list':tasks}
+    return render(request, 'task_list.html', context)
